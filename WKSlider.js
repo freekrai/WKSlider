be263882f9344dac82629b5cf02056b84d7a507d
Alex Gibson <a.gibson@me.com>
2012-02-08 17:28:19 +0000

    tweak offset width

--- WKSlider.js
+++ WKSlider.js
@@ -3,7 +3,7 @@
  * Find more about this plugin by visiting
  * http://miniapps.co.uk/
  *
- * Copyright (c) 2010 Alex Gibson, http://miniapps.co.uk/
+ * Copyright (c) 2010 - 2012 Alex Gibson, http://miniapps.co.uk/
  * Released under MIT license 
  * http://miniapps.co.uk/license/
  * 
@@ -11,9 +11,9 @@
  
 (function() {
 
-	function WKSlider(id) {
+	function WKSlider(el) {
 
-		this.element = document.getElementById(id);
+		this.element = typeof el === 'object' ? el : document.getElementById(el);
 		
 		//detect support for Webkit CSS 3d transforms
 		this.supportsWebkit3dTransform = ('WebKitCSSMatrix' in window && 'm11' in new WebKitCSSMatrix());
@@ -25,96 +25,80 @@
 		//get track width
 		this.track = this.element.getElementsByClassName('track')[0];
 		this.trackWidth = this.track.offsetWidth;
+		this.trackOffset = this.track.offsetLeft;
 	
 		this.element.addEventListener('touchstart', this, false);
 		this.element.addEventListener('mousedown', this, false);
 	}
 
 	WKSlider.prototype.touchstart = function(e) {
-
 		e.preventDefault();
-
 		this.moveKnobTo(e.targetTouches[0].pageX);
-	
 		this.element.addEventListener('touchmove', this, false);
 		this.element.addEventListener('touchend', this, false);
 		this.element.addEventListener('touchcancel', this, false);
 	};
 
 	WKSlider.prototype.touchmove = function(e) {
-
 		e.preventDefault();
-	
 		this.moveKnobTo(e.targetTouches[0].pageX);
 	};
 
 	WKSlider.prototype.touchend = function(e) {
-
 		e.preventDefault();
-
 		this.moveKnobTo(e.changedTouches[0].pageX);
-	
 		this.element.removeEventListener('touchmove', this, false);
 		this.element.removeEventListener('touchend', this, false);
 		this.element.removeEventListener('touchcancel', this, false);
 	};
 
 	WKSlider.prototype.touchcancel = function(e) {
-
 		e.preventDefault();
-
 		this.moveKnobTo(e.changedTouches[0].pageX);
-	
 		this.element.removeEventListener('touchmove', this, false);
 		this.element.removeEventListener('touchend', this, false);
 		this.element.removeEventListener('touchcancel', this, false);
 	};
 
 	WKSlider.prototype.mousedown = function(e) {
-
 		e.preventDefault();
-
 		this.moveKnobTo(e.clientX);
-	
 		this.element.addEventListener('mousemove', this, false);
 		this.element.addEventListener('mouseup', this, false);
 	};
 
 	WKSlider.prototype.mousemove = function(e) {
-
 		e.preventDefault();
-	
 		this.moveKnobTo(e.pageX);
 	};
 
 	WKSlider.prototype.mouseup = function(e) {
-
 		e.preventDefault();
-
 		this.moveKnobTo(e.pageX);
-	
 		this.element.removeEventListener('mousemove', this, false);
 		this.element.removeEventListener('mouseup', this, false);
 	};
 
 	//moves the slider
 	WKSlider.prototype.moveKnobTo = function(x) {
-	
-		x = x - this.element.offsetLeft;
-		x = Math.min(x, this.trackWidth);
-		x = Math.max(x - this.knobWidth, 0);
+
+		var percentage = 0,
+			pos = 0;
+
+		pos = (x - this.trackOffset + this.knobWidth / 2);
+		pos = Math.min(pos, this.trackWidth);
+		pos = Math.max(pos - this.knobWidth, 0);
 	
 		//use Webkit CSS 3d transforms for hardware acceleration if available 
 		if (this.supportsWebkit3dTransform) {
-		
-			this.knob.style.webkitTransform = 'translate3d(' + x + 'px, 0, 0)';
+			this.knob.style.webkitTransform = 'translate3d(' + pos + 'px, 0, 0)';
 		}
 		else {
-			this.knob.style.webkitTransform = this.knob.style.MozTransform = this.knob.style.msTransform = this.knob.style.OTransform = this.knob.style.transform = 'translateX(' + x + 'px)';
+			this.knob.style.webkitTransform = this.knob.style.MozTransform = this.knob.style.msTransform = this.knob.style.OTransform = this.knob.style.transform = 'translateX(' + pos + 'px)';
 		}
 	
 		//return value change as a percentage
-		var percentage = Math.round(x  / (this.trackWidth - this.knobWidth) * 100);
+		percentage = Math.round(pos  / (this.trackWidth - this.knobWidth) * 100);
 		this.callback(percentage);
 	};
 
